c074a2e224c872aa4de9656ec3dfaa74ea940ec9
LukeTseng <131450571+LukeTsengTW@users.noreply.github.com>
2024-07-08 21:30:30 +0800

    Update Arduino_RFID_check_in_machine.ino

--- Arduino_RFID_check_in_machine.ino
+++ Arduino_RFID_check_in_machine.ino
@@ -1,14 +1,13 @@
 /*
-
 Arduino_RFID_check_in_machine
 
 Author : LukeTseng ( 鼓山高中 _ 電腦社社長 )
 Date : 2024 / 04 / 11 初製作
 
-Version : 1.0.0 版本
+Version : 1.0.1 版本
 
-Update : None
- 
+Update : 簡化及優化程式碼片段，使其具有良好的維護性。
+
 */
 #include <SPI.h>  // Arduino 內建函式庫
 #include <MFRC522.h>  // RFID RC522 函式庫
@@ -63,34 +62,8 @@ void buzz(int sec) {  // 定義函數 buzz 用於簡化蜂鳴器程式碼
     delay(sec);
     digitalWrite(BUZZER_PIN, LOW);
 }
- 
-void setup() {
 
-  pinMode(BUZZER_PIN, OUTPUT); // 蜂鳴器預設輸出
-  digitalWrite(BUZZER_PIN, LOW); // 蜂鳴器預設低電位(不發出聲音)
-  
-  for (int i = IDENTIFY_PIN; i <= RESET_PIN; i++){
-    pinMode(i, INPUT); // 按鈕預設輸入
-    digitalWrite(i, HIGH); // 按鈕預設高電位
-  }
-
-  SPI.begin();
-  
-  mfrc522.PCD_Init();       // 初始化MFRC522讀卡機模組
-
-  lcd.begin(16,2); // 指定 LCD 尺寸像素格 16 x 2
-  lcd.backlight();
-  delay(1000); // 延時一秒後繼續
-
-  lcd.setCursor(0,0);
-  lcd.print("GuShan Check in");   // 初始化訊息
-  lcd.setCursor(0,1);
-  lcd.print("Nice to meet you");
-  delay(3000); // 延時三秒後繼續
-  lcd.clear();
-}
- 
-void loop() {
+void check_buttons() {
     for (int i = 0; i < 3; i++) {
         if (digitalRead(PINS[i]) == LOW) {
             switch (i) {
@@ -109,68 +82,103 @@ void loop() {
             print_message(MESSAGES[i][0], MESSAGES[i][1], 2000);
         }
     }
-    // 確認是否有新卡片
-    if (Is_Identify == true){
-      byte *id = mfrc522.uid.uidByte;   // 取得卡片的UID
-      byte idSize = mfrc522.uid.size;   // 取得UID的長度
-      lcd.setCursor(0,0);
-      lcd.print("UID:");
-      lcd.setCursor(0,1);
-      if ((mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) && Is_Identify == true) {
-        for (byte i = 0; i < idSize; i++) {  // 逐一顯示UID碼
-          lcd.print(id[i], HEX);       // 以16進位顯示UID值
-          lcd.print(" ");
-        }
-        delay(5000);
-        lcd.clear();
-      }
+}
+
+void display_uid(byte *id, byte idSize) {
+    lcd.setCursor(0,0);
+    lcd.print("UID:");
+    lcd.setCursor(0,1);
+    for (byte i = 0; i < idSize; i++) {  // 逐一顯示UID碼
+        lcd.print(id[i], HEX);       // 以16進位顯示UID值
+        lcd.print(" ");
     }
-    else{
-      lcd.setCursor(0,0);
-      lcd.print("Waiting for card");
-      if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+    delay(5000);
+    lcd.clear();
+}
+
+void identify_mode() {
+    if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+        byte *id = mfrc522.uid.uidByte;   // 取得卡片的UID
+        byte idSize = mfrc522.uid.size;   // 取得UID的長度
+        display_uid(id, idSize);
+    }
+}
+
+void loading_mode() {
+    lcd.setCursor(0,0);
+    lcd.print("Waiting for card");
+    if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
         byte *id = mfrc522.uid.uidByte;   // 取得卡片的UID
         byte idSize = mfrc522.uid.size;   // 取得UID的長度
         bool foundTag = false;    // 是否找到紀錄中的標籤，預設為「否」。
         
-        // 根據卡片回應的SAK值（mfrc522.uid.sak）判斷卡片類型
-        MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
-        Serial.println();
-      
-      for (byte i = 0; i < totalTags; i++) {
-        if (memcmp(tags[i].uid, id, idSize) == 0) {  // 比對陣列資料值
-            lcd.clear();
-            lcd.setCursor(0, 0);
-            lcd.print(tags[i].name);
+        for (byte i = 0; i < totalTags; i++) {
+            if (memcmp(tags[i].uid, id, idSize) == 0) {  // 比對陣列資料值
+                lcd.clear();
+                lcd.setCursor(0, 0);
+                lcd.print(tags[i].name);
 
-            if (stu[0]) {
-                lcd.setCursor(0, 1);
-                lcd.print("U've checked in");
-                buzz(150);
-            } else {
-                lcd.setCursor(0, 1);
-                lcd.print("U check in.");
-                buzz(150);
-                delay(50);
-                buzz(150);
-                stu[0] = true;  // 設定 stu[0] 為 true
+                if (stu[0]) {
+                    lcd.setCursor(0, 1);
+                    lcd.print("U've checked in");
+                    buzz(150);
+                } else {
+                    lcd.setCursor(0, 1);
+                    lcd.print("U check in.");
+                    buzz(150);
+                    delay(50);
+                    buzz(150);
+                    stu[0] = true;  // 設定 stu[0] 為 true
+                }
+
+                delay(3000);
+                lcd.clear();
+                foundTag = true;  // 設定成「找到標籤了！」
+                break;
             }
+        }
 
+        if (!foundTag) {  // 若掃描到紀錄之外的標籤，則顯示"Wrong card!"。
+            lcd.clear();
+            lcd.print("Wrong card!");
+            buzz(500);
             delay(3000);
             lcd.clear();
-            foundTag = true;  // 設定成「找到標籤了！」
-            break;
+            mfrc522.PICC_HaltA();  // 讓卡片進入停止模式      
         }
-      }
- 
-      if (!foundTag) {  // 若掃描到紀錄之外的標籤，則顯示"Wrong card!"。
-        lcd.clear();
-        lcd.print("Wrong card!");
-        buzz(500);
-        delay(3000);
-        lcd.clear();
-        mfrc522.PICC_HaltA();  // 讓卡片進入停止模式      
-        }
-      } 
+    }
+}
+
+void setup() {
+    pinMode(BUZZER_PIN, OUTPUT); // 蜂鳴器預設輸出
+    digitalWrite(BUZZER_PIN, LOW); // 蜂鳴器預設低電位(不發出聲音)
+    
+    for (int i = IDENTIFY_PIN; i <= RESET_PIN; i++) {
+        pinMode(i, INPUT); // 按鈕預設輸入
+        digitalWrite(i, HIGH); // 按鈕預設高電位
+    }
+
+    SPI.begin();
+    mfrc522.PCD_Init();       // 初始化MFRC522讀卡機模組
+
+    lcd.begin(16,2); // 指定 LCD 尺寸像素格 16 x 2
+    lcd.backlight();
+    delay(1000); // 延時一秒後繼續
+
+    lcd.setCursor(0,0);
+    lcd.print("GuShan Check in");   // 初始化訊息
+    lcd.setCursor(0,1);
+    lcd.print("Nice to meet you");
+    delay(3000); // 延時三秒後繼續
+    lcd.clear();
+}
+
+void loop() {
+    check_buttons();
+
+    if (Is_Identify) {
+        identify_mode();
+    } else {
+        loading_mode();
     }
 }
